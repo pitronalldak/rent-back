7106010a8c41b3a74e8241fb03d84b64e6279bdc
pitronalldak <ryabinin.p.b@gmail.com>
2017-01-25 01:21:11 +0300

    Adder CORS configurations

--- src/index.js
+++ src/index.js
@@ -20,10 +20,11 @@ app.server = http.createServer(app);
 /**
  *  3rd party middleware.
  */
-app.use(cors({
-    origin: '*',
-    optionsSuccessStatus: 200
-}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(bodyParser.json({
 	limit : config.bodyLimit
