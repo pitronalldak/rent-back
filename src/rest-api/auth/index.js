12a6e5a0b1672c99cab81079423af0e60c2f3e9b
pitronalldak <ryabinin.p.b@gmail.com>
2017-01-27 17:56:10 +0300

    Added isLogin flag

--- src/rest-api/auth/index.js
+++ src/rest-api/auth/index.js
@@ -13,6 +13,7 @@ export default class AuthApi {
     register = () => {
         this.app.post('/auth/sign-up', (req, res) => this.service.createUser(req, res));
         this.app.post('/auth/login', (req, res) => this.service.login(req, res));
+        this.app.get('/auth/logout', (req, res) => this.service.logout(req, res));
         this.app.get('/auth/user', (req, res) => this.service.getUser(req, res));
     }
 }
